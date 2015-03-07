fab54ee49674a8a860bf1880bf2be3e932e428f5
DraperStudio <hello@draperstud.io>
2015-03-07 12:01:21 +0100

    Update comments

--- src/SlackExtendSocialite.php
+++ src/SlackExtendSocialite.php
@@ -6,7 +6,9 @@ use SocialiteProviders\Manager\SocialiteWasCalled;
 class SlackExtendSocialite
 {
     /**
-     * Execute the provider.
+     * Register the provider.
+     *
+     * @param \SocialiteProviders\Manager\SocialiteWasCalled $socialiteWasCalled
      */
     public function handle(SocialiteWasCalled $socialiteWasCalled)
     {
